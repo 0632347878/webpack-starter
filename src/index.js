a2113645800eade04e2f944538cbf5ac6d44deae
denis.vyshkevich <denis.vyshkevich@branderstudio.com>
2020-02-28 16:55:50 +0200

    clean pack

--- src/index.js
+++ src/index.js
@@ -1,37 +1,7 @@
-// import groupBy from "lodash-es"
-import { sortBy, groupBy } from "lodash-es"
-import people from './people'
-
-// const managerGroups = groupBy(people, "manager");
-const managersSorted = sortBy(people, function (item) {
-  return item.id;
-});
-
-const root = document.createElement("div");
-// root.innerHTML = `<pre>${JSON.stringify(managerGroups, null, 2)}</pre>`;
-root.innerHTML = `<pre>${JSON.stringify(managersSorted, null, 2)}</pre>`;
-document.body.appendChild(root);
-// console.log(managerGroups);
-
-let user = {
-  name: "John",
-  age: 30,
-  isAdmin: true
-};
-
-for (let key in user) {
- key === 'name' ? Object.defineProperty(user, "name", { writable: false }) : null ;
- // console.log( key + ' - descriptior: ', Object.getOwnPropertyDescriptor(user, key));
+function sum(x, y, z) {
+  return x + y + z;
 }
 
-let clone = {
-  'first field': 'one'
-};
+const numbers = [1, 2, 3];
 
-for (let key in user) {
-  clone[key] = user[key];      
-}
-
-clone.name = 'empty';
-
-// console.log(clone, user);
+console.log(sum(numbers));
